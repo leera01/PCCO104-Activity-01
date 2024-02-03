cc51838d09f48cade36a2aefd388e85891cc9136
leera01 <leera01@users.noreply.github.com>
2024-02-03 02:44:27 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string firstName = "Ralph";
+    Console.WriteLine ($"Hello World {firstName}");
+    Console.WriteLine ("Hello World " +  firstName);
+  }
+}
